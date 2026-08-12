1222f2c46657cbcc340ea9b9b48966a23801b073
thiennguyen3004931-sudo <thiennguyen3004931@gmail.com>
2026-08-12 20:11:13 +0700

    export phase5 forward holdout service

--- packages/risk-engine/src/services/index.ts
+++ packages/risk-engine/src/services/index.ts
@@ -8,6 +8,7 @@ export * from "./Phase4WalkForwardService";
 export * from "./Phase4ShadowDiagnosticsService";
 export * from "./Phase4ContributionDiagnosticsService";
 export * from "./Phase4SubgroupStabilityService";
+export * from "./Phase5ForwardHoldoutService";
 export * from "./ExposureService";
 export * from "./MarginService";
 export * from "./PositionSizeService";
