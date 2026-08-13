786e98430bc274c5ecb2bb35b2645c418efa980d
thiennguyen3004931-sudo <thiennguyen3004931@gmail.com>
2026-08-13 12:14:59 +0700

    Export Phase 7 trend-rider service

--- packages/risk-engine/src/services/index.ts
+++ packages/risk-engine/src/services/index.ts
@@ -15,6 +15,7 @@ export * from "./Phase6BRescueOutcomeService";
 export * from "./Phase6CForwardHoldoutService";
 export * from "./Phase6DForwardHoldoutService";
 export * from "./Phase6EHistoricalBlindService";
+export * from "./Phase7TrendRiderService";
 export * from "./ExposureService";
 export * from "./MarginService";
 export * from "./PositionSizeService";
