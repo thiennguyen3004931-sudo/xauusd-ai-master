900bdcef27f96fdca8a46631c96f8a1fdde2c5bd
thiennguyen3004931-sudo <thiennguyen3004931@gmail.com>
2026-08-27 18:56:23 +0700

    feat: add execution authorization to account risk page

--- apps/web/src/pages/Phase7CAccountRiskPage.tsx
+++ apps/web/src/pages/Phase7CAccountRiskPage.tsx
@@ -1,11 +1,13 @@
 import { Stack } from "@mui/material";
 import { Phase7BOpsPage } from "./Phase7BOpsPage";
 import { Phase7CAccountSwitchCard } from "../ui/Phase7CAccountSwitchCard";
+import { Phase7CExecutionAuthorizationCard } from "../ui/Phase7CExecutionAuthorizationCard";
 
 export function Phase7CAccountRiskPage() {
   return (
     <Stack spacing={3}>
       <Phase7CAccountSwitchCard />
+      <Phase7CExecutionAuthorizationCard />
       <Phase7BOpsPage />
     </Stack>
   );
