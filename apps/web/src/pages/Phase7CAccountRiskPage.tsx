9aa71bf6df5c886e6363b266909033225fd599dc
thiennguyen3004931-sudo <thiennguyen3004931@gmail.com>
2026-08-29 07:23:26 +0700

    feat(web): mount strategy entry conditions card

--- apps/web/src/pages/Phase7CAccountRiskPage.tsx
+++ apps/web/src/pages/Phase7CAccountRiskPage.tsx
@@ -1,11 +1,13 @@
 import { Stack } from "@mui/material";
 import { Phase7BOpsPage } from "./Phase7BOpsPage";
 import { Phase7CAccountSwitchCard } from "../ui/Phase7CAccountSwitchCard";
+import { Phase7CStrategyEntryConditionsCard } from "../ui/Phase7CStrategyEntryConditionsCard";
 
 export function Phase7CAccountRiskPage() {
   return (
     <Stack spacing={3}>
       <Phase7CAccountSwitchCard />
+      <Phase7CStrategyEntryConditionsCard />
       <Phase7BOpsPage />
     </Stack>
   );
