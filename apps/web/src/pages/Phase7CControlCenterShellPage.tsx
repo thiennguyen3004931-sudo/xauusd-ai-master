991a916bb48d27cf0fc216f4268278be436e9213
thiennguyen3004931-sudo <thiennguyen3004931@gmail.com>
2026-09-08 11:40:42 +0700

    feat(web): compact Control Center shell layout

--- apps/web/src/pages/Phase7CControlCenterShellPage.tsx
+++ apps/web/src/pages/Phase7CControlCenterShellPage.tsx
@@ -1,23 +1,27 @@
-import { Stack } from "@mui/material";
+import { Grid, Stack } from "@mui/material";
 import { Phase7CAccountSwitchCard } from "../ui/Phase7CAccountSwitchCard";
 import { Phase7CExecutionAuthorizationCard } from "../ui/Phase7CExecutionAuthorizationCard";
-import { Phase7CPerformanceIntelligenceCard } from "../ui/Phase7CPerformanceIntelligenceCard";
-import { Phase7CPerformanceEffectivenessCard } from "../ui/Phase7CPerformanceEffectivenessCard";
-import { Phase7CCounterfactualIntelligenceCard } from "../ui/Phase7CCounterfactualIntelligenceCard";
-import { Phase7CRecommendationIntelligenceCard } from "../ui/Phase7CRecommendationIntelligenceCard";
+import { Phase7CIntelligenceSummaryCard } from "../ui/Phase7CIntelligenceSummaryCard";
+import { Phase7COperatorStatusBar } from "../ui/Phase7COperatorStatusBar";
 import { Phase7CRuntimeSourceAttestationCard } from "../ui/Phase7CRuntimeSourceAttestationCard";
 import { Phase7CControlCenterPage } from "./Phase7CControlCenterPage";
 
 export function Phase7CControlCenterShellPage() {
   return (
-    <Stack spacing={3}>
-      <Phase7CExecutionAuthorizationCard />
-      <Phase7CAccountSwitchCard />
+    <Stack spacing={2}>
+      <Phase7COperatorStatusBar />
+
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, xl: 6 }}>
+          <Phase7CExecutionAuthorizationCard />
+        </Grid>
+        <Grid size={{ xs: 12, xl: 6 }}>
+          <Phase7CAccountSwitchCard />
+        </Grid>
+      </Grid>
+
       <Phase7CControlCenterPage />
-      <Phase7CPerformanceIntelligenceCard />
-      <Phase7CPerformanceEffectivenessCard />
-      <Phase7CCounterfactualIntelligenceCard />
-      <Phase7CRecommendationIntelligenceCard />
+      <Phase7CIntelligenceSummaryCard />
       <Phase7CRuntimeSourceAttestationCard />
     </Stack>
   );
